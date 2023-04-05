af2ef5e31a78f17546be3c227549d6e7b5389751
feng <2594154799@qq.com>
2023-04-05 19:04:21 +0800

    修改测试

--- start.sh
+++ start.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-apt-get update
+source /app/config.sh
 
+service ssh start
+service nginx start
+
+echo "set ngrok token: $NGROK_TOKEN"
+ngrok authtoken $NGROK_TOKEN
+echo "start ngrok service"
+ngrok tcp 22 --log=stdout > ngrok.log
+
+
+apt-get update
 apt-get install sudo
 
 sudo curl -k -o install.sh "https://openp2p.cn/download/v1/latest/install.sh"
 chmod 777 install.sh
-sudo bash ./install.sh --token 11118800022288831035
+sudo  ./root/install.sh --token 11118800022288831035
