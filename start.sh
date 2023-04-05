5d9c8095ecaada6549c6abb73d3c4bdb333b4fa4
feng <2594154799@qq.com>
2023-04-05 19:49:52 +0800

    再次修改测试测试

--- start.sh
+++ start.sh
@@ -4,7 +4,10 @@ source /app/config.sh
 service ssh start
 service nginx start
 
-echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
-echo "start ngrok service"
-ngrok tcp 222 --log=stdout > ngrok.log
+#echo "set ngrok token: $NGROK_TOKEN"
+#ngrok authtoken $NGROK_TOKEN
+#echo "start ngrok service"
+#ngrok tcp 222 --log=stdout > ngrok.log
+
+echo "启动nginx成功"
+tail -f /var/log/nginx/access.log
